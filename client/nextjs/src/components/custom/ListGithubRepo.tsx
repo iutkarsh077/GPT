840020cce997bfe86fceb1a14c80abdff077f82e
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-07-26 10:05:16 +0530

    show github repo list

--- client/nextjs/src/components/custom/ListGithubRepo.tsx
+++ client/nextjs/src/components/custom/ListGithubRepo.tsx
@@ -23,6 +23,7 @@ type ListGithubRepoProps = {
     setRepos: Dispatch<SetStateAction<IGithubRepo[] | null>>;
 };
 
+// This will show the list of github repos and allow the user to toggle the code review on/off
 const ListGithubRepo = ({ isOpen, onClose, repos, isLoading, setRepos }: ListGithubRepoProps) => {
 
 
